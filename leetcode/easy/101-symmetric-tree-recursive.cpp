2042b9908c0b6bd3efb1f0ce23faaa4cdfe2f4e6
IssamLRB <laribiissam8@gmail.com>
2025-08-31 16:16:53 +0100

    Update 101-symmetric-tree-recursive.cpp

--- leetcode/easy/101-symmetric-tree-recursive.cpp
+++ leetcode/easy/101-symmetric-tree-recursive.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if (!root) return true;
-        return isMirror(root->left, root->right);
+        if (!root) return true; // If the tree is empty, it is symmetric
+        return isMirror(root->left, root->right); // Left and right subtrees comparison
     }
 
 private:
     // Recursive helper
     bool isMirror(TreeNode* t1, TreeNode* t2) {
-        if (!t1 && !t2) return true;
-        if (!t1 || !t2) return false;
-        return (t1->val == t2->val) 
-            && isMirror(t1->left, t2->right) 
-            && isMirror(t1->right, t2->left);
+        if (!t1 && !t2) return true; // If both are null, it is symmetric
+        if (!t1 || !t2) return false; // If only one is null, it is not symmetric
+        
+        return (t1->val == t2->val) // vlues are the same
+            && isMirror(t1->left, t2->right) // t1 left and t2 right comparison
+            && isMirror(t1->right, t2->left); // t1 right and t2 left comparison
     }
 };
 
