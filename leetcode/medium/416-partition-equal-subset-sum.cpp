d4abbbc31e9e5b884156e9e49c2434866c488794
IssamLRB <laribiissam8@gmail.com>
2025-09-04 00:24:28 +0100

    Update 416-partition-equal-subset-sum.cpp

--- leetcode/medium/416-partition-equal-subset-sum.cpp
+++ leetcode/medium/416-partition-equal-subset-sum.cpp
@@ -8,7 +8,24 @@ private:
 
 public:
     bool canPartition(vector<int>& nums) {
-        int totalSum = 
-        return false;
+        int target = sum(nums);
+        // If total sum is odd, cannot partition into two equal subsets
+        if (target % 2 == 1) return false;
+        
+        // we just need to see if there's a subset with this sum
+        target /= 2;
+        
+        bool possible[target + 1];
+        possible[0] = true; // base case: sum 0 is always possible
+        for (int i = 1; i < target + 1; i++) possible[i] = false;
+
+        for (int n : nums) {
+            for (int i = target; i >= n; i--) {
+                if (possible[i - n]) {
+                    possible[i] = true;
+                }
+            }
+        }
+        return possible[target];
     }
 };
