2bd388d123f2d10782e0962be1268d1e362ceeed
IssamLRB <laribiissam8@gmail.com>
2025-09-04 00:00:11 +0100

    Update 416-partition-equal-subset-sum.cpp

--- leetcode/medium/416-partition-equal-subset-sum.cpp
+++ leetcode/medium/416-partition-equal-subset-sum.cpp
@@ -1,1 +1,14 @@
+class Solution {
+private: 
+    int sum(vector<int> nums) {
+        int sum = 0;
+        for (int n : nums) sum += n;
+        return sum;
+    }
 
+public:
+    bool canPartition(vector<int>& nums) {
+        int totalSum = 
+        return false;
+    }
+};
