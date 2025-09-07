c1879147ab55ed59ec3305dc0bc9fa259cb81338
IssamLRB <laribiissam8@gmail.com>
2025-09-07 03:12:36 +0100

    Update 14-longest-common-prefix.py

--- leetcode/easy/14-longest-common-prefix.py
+++ leetcode/easy/14-longest-common-prefix.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        # Compare characters column by column
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for s in strs[1:]:
+                # If index out of range or character mismatch
+                if i >= len(s) or s[i] != char:
+                    return strs[0][:i]
+        
+        return strs[0]  # All characters matched
