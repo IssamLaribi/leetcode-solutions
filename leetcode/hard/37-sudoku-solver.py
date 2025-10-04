9dba87fb4097562093a009dd0f41fae05ef2a922
IssamLRB <laribiissam8@gmail.com>
2025-10-04 20:29:50 +0100

    Update 37-sudoku-solver.py

--- leetcode/hard/37-sudoku-solver.py
+++ leetcode/hard/37-sudoku-solver.py
@@ -1,37 +1,42 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        
-        def is_valid(r, c, ch):
-            # check row
-            for i in range(9):
-                if board[r][i] == ch:
-                    return False
-            # check column
-            for i in range(9):
-                if board[i][c] == ch:
-                    return False
-            # check 3x3 sub-box
-            box_row, box_col = 3 * (r // 3), 3 * (c // 3)
-            for i in range(box_row, box_row + 3):
-                for j in range(box_col, box_col + 3):
-                    if board[i][j] == ch:
-                        return False
-            return True
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empties = []
 
-        def backtrack():
-            for r in range(9):
-                for c in range(9):
-                    if board[r][c] == ".":
-                        for ch in map(str, range(1, 10)):
-                            if is_valid(r, c, ch):
-                                board[r][c] = ch
-                                if backtrack():
-                                    return True
-                                board[r][c] = "."
-                        return False
-            return True
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == ".":
+                    empties.append((r, c))
+                else:
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[(r // 3) * 3 + (c // 3)].add(val)
+
+        def backtrack(i=0):
+            if i == len(empties):
+                return True
+
+            r, c = empties[i]
+            b = (r // 3) * 3 + (c // 3)
+
+            for ch in map(str, range(1, 10)):
+                if ch not in rows[r] and ch not in cols[c] and ch not in boxes[b]:
+                    board[r][c] = ch
+                    rows[r].add(ch)
+                    cols[c].add(ch)
+                    boxes[b].add(ch)
+
+                    if backtrack(i + 1):
+                        return True
+
+                    board[r][c] = "."
+                    rows[r].remove(ch)
+                    cols[c].remove(ch)
+                    boxes[b].remove(ch)
+
+            return False
 
         backtrack()
